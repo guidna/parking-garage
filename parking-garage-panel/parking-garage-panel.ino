01b4414f0d3c2838a54fd16e9b608f96fef80d14
Guilherme Dantas <guilhermedna@hotmail.com>
2017-10-31 22:13:25 -0200

    Adicionei funções de LED vaga disponível, status MQTT e sensor ultrassom

--- parking-garage-panel/parking-garage-panel.ino
+++ parking-garage-panel/parking-garage-panel.ino
@@ -9,10 +9,29 @@ String msgNoVacancy  = "NAO HA VAGAS";
 String msgVacancy    = "Vagas disp: ";
 String msgCnxErro    = "System out";
 
+#include <Ultrasonic.h>
+
+Ultrasonic ultrasonic(12, 13);
+
+boolean previousStatusVacancy;
+boolean currentStatusVacancy;
+String statusVacancy = "";
+
+const int vacancyStatusLED = 7;
+
+const int mqttStatusOnLED = 6;
+const int mqttStatusOffLED = 5;
+
 void setup()
 {
   serialSetup();
   lcdSetup();
+
+  //LEDs vaga disponível e MQTT status
+  pinMode(vacancyStatusLED, OUTPUT);
+  pinMode(mqttStatusOnLED, OUTPUT);
+  pinMode(mqttStatusOffLED, OUTPUT);
+  
 }
 
 void loop()
@@ -21,6 +40,18 @@ void loop()
   showVacancy(qtdVacancy);
   lcdBlink(qtdVacancy);
 
+//A função abaixo recebe o boolean mqttStatus
+  showMqttStatusLED(0);
+  
+  verifyStatusVacancy();
+      
+  if(currentStatusVacancy != previousStatusVacancy){
+    
+    printStatusVacancy();
+
+    previousStatusVacancy = currentStatusVacancy;
+    
+  }
 }
 
 void serialSetup() {
@@ -75,5 +106,38 @@ void lcdBlink(int vagas) {
     delay(1000);
   }
 
+}
+
+void printStatusVacancy() {
+
+  if (currentStatusVacancy){
+    statusVacancy = "livre";
+    digitalWrite(vacancyStatusLED,HIGH);
+  }else{
+    statusVacancy = "ocupada";
+    digitalWrite(vacancyStatusLED,LOW);
+  }
+  Serial.print("Vaga: ");
+  Serial.println(statusVacancy);
+
+}
+
+void verifyStatusVacancy() {
+
+  int distancia = ultrasonic.distanceRead();
+
+  if (distancia > 50) {
+    currentStatusVacancy = true;
+  } else {
+    currentStatusVacancy = false;
+  }
+  
+}
+
+void showMqttStatusLED (boolean mqttStatus){
+
+  digitalWrite(mqttStatusOnLED, mqttStatus);
+  digitalWrite(mqttStatusOffLED, !mqttStatus);
+  
 }
 
